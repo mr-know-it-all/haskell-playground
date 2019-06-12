937184125e844287dff8203f2cd8029a42fb40a8
Paul <paul.constantin.chitic@gmail.com>
2019-06-12 22:10:45 +0300

    add filter and reduce for custom recursive list

--- recursive-types/list.hs
+++ recursive-types/list.hs
@@ -9,3 +9,13 @@ length' (Cons _ xs) = 1 + (length' xs)
 map' :: (a -> b) -> List a -> List b
 map' _ Empty = Empty
 map' fn (Cons x xs) = Cons (fn x) (map' fn xs)
+
+filter' :: (a -> Bool) -> List a -> List a
+filter' _ Empty = Empty
+filter' pred (Cons x xs)
+  | pred x = Cons x (filter' pred xs)
+  | otherwise = filter' pred xs
+
+reduce' :: t1 -> (t2 -> t1 -> t1) -> List t2 -> t1
+reduce' init _ Empty = init
+reduce' init fn (Cons x xs) = fn x (reduce' init fn xs)
--- recursive-types/tests.hs
+++ recursive-types/tests.hs
@@ -2,13 +2,45 @@ import Test.HUnit
 import List
 import Prelude
 
-test_one = TestCase $ assertEqual "expecting length of 2" 2 (length' (Cons 1 (Cons 2 Empty)))
-test_two = TestCase $ assertEqual "expecting map to work" (Cons 2 (Cons 3 Empty)) $ map' (\x -> x + 1) (Cons 1 (Cons 2 Empty))
+test_length =
+  TestCase $ assertEqual
+    "expecting length of 2"
+    2
+    (length' (Cons 1 (Cons 2 Empty)))
+test_map =
+  TestCase $ assertEqual
+    "expecting map to work"
+    (Cons 2 (Cons 3 Empty))
+    $ map' (\x -> x + 1) (Cons 1 (Cons 2 Empty))
+test_filter =
+  TestCase $ assertEqual
+    "expecting filter to work"
+    (Cons 2 Empty)
+    $ filter' (\x -> x > 1) (Cons 1 (Cons 2 Empty))
+test_reduce_sum =
+  TestCase $ assertEqual
+    "expecting reduce to work for sum"
+    42
+    $ reduce' 0 (\acc v -> acc + v) (Cons 40 (Cons 2 Empty))
+test_reduce_product =
+  TestCase $ assertEqual
+    "expecting reduce to work for product"
+    160
+    $ reduce' 1 (\acc v -> acc * v) (Cons 40 (Cons 2 (Cons 2 Empty)))
+test_reduce_concat =
+  TestCase $ assertEqual
+    "expecting reduce to work for concat"
+    "abc"
+    $ reduce' "" (\acc v -> acc ++ v) (Cons "a" (Cons "b" (Cons "c" Empty)))
 
 tests_custom_list =
   TestList [
-    TestLabel "test list length" test_one,
-    TestLabel "test list map" test_two
+    TestLabel "test list length" test_length,
+    TestLabel "test list map" test_map,
+    TestLabel "test list filter" test_filter,
+    TestLabel "test list reduce with sum" test_reduce_sum,
+    TestLabel "test list reduce with product" test_reduce_product,
+    TestLabel "test list reduce with concat" test_reduce_concat
   ]
 
 main = do
